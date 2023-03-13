e361195548eb90ff94f360404647f76e44c6d28d
Simran <vermasimran261@gmail.com>
2023-03-13 17:18:41 +0530

    Initial Commit

---
+++ JS/script.js
@@ -0,0 +1,22 @@
+// $(document).ready(function(){
+//     $('#hero-slider').owlCarousel({
+//         loop:true,
+//         margin:0,
+//         nav:true,
+//         items: 1,
+            // dots:false,
+            // smartSpeed: 1000,
+            //navText : ['PREV','NEXT'],
+//         responsive:{
+//             0:{
+//                 items:1
+//             },
+//             600:{
+//                 items:3
+//             },
+//             1000:{
+//                 items:5
+//             }
+//         }
+//     })
+// });
